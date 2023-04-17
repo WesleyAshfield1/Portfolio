8ad121fc02b9e3c5628d71e17a9e6b73f3512028
Wesley Ashfield <lwashfield@gmail.com>
2023-04-17 14:35:35 -0400

    updated projects , componets

--- src/pages/AboutMe/index.js
+++ src/pages/AboutMe/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Container } from "react-bootstrap";
-import 'bootstrap/dist/css/bootstrap.min.css';
 import MyPic from './../../assets/images/MyPic.PNG';
 
 
--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import 'bootstrap/dist/css/bootstrap.min.css'
 
 const Contact = () => {
   return (
